2259994711438ba85e7b48150f4edcdabd792228
JakeGinesin <wendellthenerd123@gmail.com>
2022-10-21 16:44:38 -0400

    jcompile.sh

--- .scripts/jcompile.sh
+++ .scripts/jcompile.sh
@@ -9,8 +9,9 @@ rm manifest.txt
 echo "Main-Class: ${1}" > manifest.txt
 
 # removing previously compiled files
-rm -r */*/*.class
+find . -name "*.class" -type f -delete
 
+# advanced:
 # find all java files and output the names to sources.txt
 find -name "*.java" > sources.txt
 
@@ -23,23 +24,4 @@ echo "---------- COMPILATION BEGIN ----------"
 # compile all!
 javac @sources.txt
 
-# get compiled files and now output to sources.txt
-find -name "*.class" > sources.txt
-
-# create jar
-jar cvfm Program.jar manifest.txt @sources.txt
-
-# get contents of jar
-jar tf Program.jar
-
-# run jar
-java -jar Program.jar
-
 echo "---------- COMPILATION END ----------"
-
-printf "\n"
-
-echo "---------- JUNIT TESTING BEGIN ----------"
-
-java org.junit.runner.JUnitCore $1
-echo "---------- JUNIT TESTING END ----------"
