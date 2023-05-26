adc91a7d8e85ec5173d95a6c089dfa263c3d2760
JakeGinesin <wendellthenerd123@gmail.com>
2023-05-26 00:49:11 -0400

    vimwiki stuff

---
+++ .scripts/vimwiki/open_daily.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this script is invoked to launch my main daily note
+# https://jakegines.in/blog/2023/one
+
+# yeah yeah I need to replace this with a function in init.vim but i am lazy
+# I also really need to better organize my neovim config lmao but idc
+alacritty -t "Daily.md" -e nvim /home/synchronous/Documents/Obsidian/Journal/Daily.md -c "execute 'normal G' | nnoremap <CR> :call CustomGf()<CR> | nnoremap gf :call CustomGf()<CR> | syntax match LinkPattern /[a-zA-Z0-9][a-zA-Z0-9]*\.md/ | highlight LinkPattern guifg=LightBlue gui=underline | set path+=/home/synchronous/Documents/Obsidian/Journal/abstract | Goyo"
---
+++ .scripts/vimwiki/search_notes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# this script is designed to select a note to open via rofi
+
+rs="$(find /home/synchronous/Documents/Obsidian/Journal/abstract -follow | cut -c55- | grep -E '.md$' | grep -v 'template')"
+rcv=$(echo "$rs" | rofi -dmenu)
+if [[ "$rcv" == "" ]]; then exit 1; fi
+if [[ $(echo "$rs" | grep "$rcv") ]]; then
+  alacritty -t "$rcv" -e nvim /home/synchronous/Documents/Obsidian/Journal/abstract/$rcv -c "Goyo | syntax match LinkPattern /[a-zA-Z0-9][a-zA-Z0-9]*\.md/ | highlight LinkPattern guifg=LightBlue gui=underline | nnoremap <CR> :call CustomGf()<CR> | nnoremap gf :call CustomGf()<CR> | set path+=/home/synchronous/Documents/Obsidian/Journal/abstract"
+else
+  # create a new file
+  
+  # if rcv doesn't contain md, add it
+  if [[ $(echo "$rcv" | grep -v -E ".md$") ]]; then 
+    rcv="$rcv.md"
+  fi
+  alacritty -t "$rcv" -e nvim /home/synchronous/Documents/Obsidian/Journal/abstract/$rcv -c "Goyo | syntax match LinkPattern /[a-zA-Z0-9][a-zA-Z0-9]*\.md/ | highlight LinkPattern guifg=LightBlue gui=underline nnoremap <CR> | :call CustomGf()<CR> | nnoremap gf :call CustomGf()<CR> | set path+=/home/synchronous/Documents/Obsidian/Journal/abstract"
+fi
