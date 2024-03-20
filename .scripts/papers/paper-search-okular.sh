fd3b4f56df624d04fb394f0b127ae15bc4772407
JakeGinesin <wendellthenerd123@gmail.com>
2024-03-20 17:12:57 -0400

    more

--- .scripts/papers/paper-search-okular.sh
+++ .scripts/papers/paper-search-okular.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-paper_dir="/home/synchronous/papers"
-papers=$(find /home/synchronous/papers -name "*.pdf" -exec basename {} \;)
+paper_dir="/home/synchronous/Zotero/storage"
+papers=$(find $paper_dir -name "*.pdf" -exec basename {} \;)
 selected_file=$(echo "$papers" | rofi -dmenu -i -p "Select a paper" -theme /home/synchronous/.config/rofi/styles/prompt-papers.rasi)
 
 if [ -n "$selected_file" ]; then
-  full_path="$paper_dir/$selected_file"
+  full_path=$(find $paper_dir -name "$selected_file")
   echo "$full_path"
 
   okular "$full_path"
 fi
+
+
+
+
