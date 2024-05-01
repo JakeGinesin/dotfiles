7b1a32a3c2b9e210d6b3d83b1ea7622651606ac5
JakeGinesin <wendellthenerd123@gmail.com>
2024-05-01 03:31:01 -0400

    rofi stuff

--- .scripts/vimwiki/search_notes.sh
+++ .scripts/vimwiki/search_notes.sh
@@ -2,7 +2,7 @@
 # this script is designed to select a note to open via rofi
 
 rs="$(find /home/synchronous/journal/abstract -follow -printf "%T@ %Tc %p\n" | sort -n -r | cut -c83- | grep -E '.md$' | grep -v 'template')"
-rcv=$(echo "$rs" | rofi -dmenu)
+rcv=$(echo "$rs" | rofi -dmenu -theme /home/synchronous/.config/rofi/styles/prompt-search.rasi)
 if [[ "$rcv" == "" ]]; then exit 1; fi
 if [[ $(echo "$rs" | grep "$rcv") ]]; then
   alacritty -t "$rcv" -e nvim /home/synchronous/journal/abstract/$rcv -c "Goyo | syntax match LinkPattern /[a-zA-Z0-9\-][a-zA-Z0-9\-]*\.md/ | highlight LinkPattern guifg=LightBlue gui=underline | nnoremap <CR> :call CustomGf()<CR> | nnoremap gf :call CustomGf()<CR> | set path+=/home/synchronous/journal/abstract"
